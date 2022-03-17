b33ad7a3743c41ee6a3eca5623e71a5c32e52c5b
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-03-17 00:24:22 -0400

    Update fxtwitter_bot.py

--- fxtwitter_bot.py
+++ fxtwitter_bot.py
@@ -20,7 +20,7 @@ async def on_message(message):
         newLink = message.content.replace("twitter","fxtwitter") #appending fx into the link 
         await message.channel.send(newLink) #sending the link to the the user
 
-client.run('')
+client.run('TOKEN HERE')
 
 
 

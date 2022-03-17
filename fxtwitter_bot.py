894439dc52953cbf63caa5e3e88f586802e6589b
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-03-17 00:38:28 -0400

    Add files via upload
    added regular expressions to try and locate twitter link

--- fxtwitter_bot.py
+++ fxtwitter_bot.py
@@ -2,6 +2,7 @@
 #developed by Brandon Giang 3/16/2022
 
 import discord
+import re #going to use regex to find twitter link
 
 client = discord.Client()
 
@@ -14,30 +15,19 @@ async def on_message(message):
     if message.author == client.user: #checking to see if the message is sent by the bot itself
         return
 
-    if message.content.startswith('https://twitter.com/'):
-
+    x = re.search("Https://twitter.com")
+    if x == None:
+        return
+    else:
         channel = message.channel
         newLink = message.content.replace("twitter","fxtwitter") #appending fx into the link 
         await message.channel.send(newLink) #sending the link to the the user
 
+    #if message.content('https://twitter.com/'): #have to find a way to 
+
+        #channel = message.channel
+        #newLink = message.content.replace("twitter","fxtwitter") #appending fx into the link 
+        #await message.channel.send(newLink) #sending the link to the the user
+
 client.run('TOKEN HERE')
 
-
-
-# to do list: 
-# 1) Make the bot be able to read if the twitter link has the twitter link at all
-# 2) Have the bot delete the message
-#   | Have the bot say who posted the twitter link (prefereably without a ping)
-# 
-#
-#
-#
-#
-# Things to consider:
-# Maybe make a flag (ex: make it ![twitter link] symbol not specific)
-# if twitfix "failed to scan link " -> display an error message
-# Maybe make it so that if someone replies to a message and pings the bot it'll embed twitter link
-#
-#
-#
-#

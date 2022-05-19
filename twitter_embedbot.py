1a72dd6ef625e59cfaba3d6b26991b12f3fb61c0
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-05-19 01:36:10 -0400

    Add files via upload

--- twitter_embedbot.py
+++ twitter_embedbot.py
@@ -19,46 +19,49 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user: #checking to see if the message is sent by the bot itself
         return
+    else:
 
 #-----------------needed variables---------------------------------------
-    tweetFlag=False #if flag is false, usr didn't send a twitter link
-    multiEmbedFlg=False #if flag is false, the link only has 1 image/embed
-    newLink=""
-    usrMsg=""
-    embedArr=message.embeds #to detect multiple embeds  
-    linkArr=[] #for when multiEmbedFlg is true
-    txtFlg=False #flag to check if the tweet is purely text
+        tweetFlag=False #if flag is false, usr didn't send a twitter link
+        multiEmbedFlg=False #if flag is false, the link only has 1 image/embed
+        newLink=""
+        usrMsg=""
+        embedArr=message.embeds #to detect multiple embeds  
+        linkArr=[] #for when multiEmbedFlg is true
 #------------------------------------------------------------------------
-
-#this message parsing:
-    x = re.split("\s",message.content)
-    for words in x: #to go through the message that was sent 
-        if "vxtwitter" in words: #this means the fix has already been applied so no need to do anything
-            return
-        if "twitter.com" in words: #found twitter link
-            tweetFlag = True
-            newLink = words.replace("twitter","vxtwitter") #replaces twitter with vx twitter
-            if len(embedArr) > 1: #to check if there's more than one embed meaning there's multiple pictures
-                multiEmbedFlg = True
-                for i in embedArr:
-                    linkArr.append(i.image.url)
-                linkArr[0]=newLink # to make the first link that's sent the vxtwitter link
+        print(embedArr)
+#message parsing:
+        x = re.split("\s",message.content)
+        for words in x: # finding if a twitter link is sent, should technically be faster if there's no twitter link
+            if "twitter.com" in words:
+                tweetFlag = True
+        if tweetFlag:
+            for words in x: #to go through the message that was sent 
+                if "vxtwitter" in words: #this means the fix has already been applied so no need to do anything
+                    return
+                if "twitter.com" in words: #found twitter link
+                    newLink = words.replace("twitter","vxtwitter") #replaces twitter with vx twitter
+                    if len(embedArr) > 1: #to check if there's more than one embed meaning there's multiple pictures
+                        multiEmbedFlg = True
+                        for i in embedArr:
+                            linkArr.append(i.image.url)
+                        linkArr[0]=newLink # to make the first link that's sent the vxtwitter link
+                else:
+                    usrMsg=usrMsg+words+" "
+            await message.delete()
+            if multiEmbedFlg:
+                await message.channel.send(message.author.display_name+": "+usrMsg+linkArr[0])
+                for i in range(1,len(embedArr)):
+                    progIndicator = str(i+1)+"/"+str(len(embedArr))
+                    await message.channel.send(progIndicator+'\n'+linkArr[i])
+                print(linkArr)
+            else:
+                await message.channel.send(message.author.display_name+": "+usrMsg+'\n'+newLink)
+                #delete message from the usr who sent twitter link 
+                #message format:
+                #{usr} said [usrMsg] \n newLink
         else:
-            usrMsg=usrMsg+words+" "
-    if tweetFlag:
-        await message.delete()
-        if multiEmbedFlg:
-            await message.channel.send(message.author.display_name+": "+usrMsg+linkArr[0])
-            for i in range(1,len(embedArr)):
-                await message.channel.send(str(i+1)+"/"+str(len(embedArr)))
-                await message.channel.send(linkArr[i])
-        else:
-            await message.channel.send(message.author.display_name+": "+usrMsg+'\n'+newLink)
-            #delete message from the usr who sent twitter link 
-            #message format:
-            #{usr} said [usrMsg] \n newLink
-    else:
-        return
+                return
 
 client.run('token')
 

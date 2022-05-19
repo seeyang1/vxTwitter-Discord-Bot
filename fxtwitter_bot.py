f14ea52e57b1bd6b9159e632def22d16587d50a3
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2022-05-19 01:09:50 -0400

    Add files via upload
    Added a check and support for posts with multiple pictures

--- fxtwitter_bot.py
+++ fxtwitter_bot.py
@@ -1,6 +1,9 @@
 #bot that will send twitter links with embedded images/videos
 #developed by Brandon Giang 3/16/2022
 
+from asyncio.windows_events import NULL
+from audioop import mul
+from queue import Empty
 import discord
 import re #going to use regex to find twitter link
 
@@ -19,29 +22,43 @@ async def on_message(message):
 
 #-----------------needed variables---------------------------------------
     tweetFlag=False #if flag is false, usr didn't send a twitter link
+    multiEmbedFlg=False #if flag is false, the link only has 1 image/embed
     newLink=""
     usrMsg=""
+    embedArr=message.embeds #to detect multiple embeds  
+    linkArr=[] #for when multiEmbedFlg is true
+    txtFlg=False #flag to check if the tweet is purely text
 #------------------------------------------------------------------------
-
-#Check for video embed here:
 
 #this message parsing:
     x = re.split("\s",message.content)
     for words in x: #to go through the message that was sent 
-        if "fxtwitter" in words: #this means the fix has already been applied so no need to do anything
+        if "vxtwitter" in words: #this means the fix has already been applied so no need to do anything
             return
-        if "twitter.com" in words:
-            tweetFlag=True
-            newLink = words.replace("twitter","vxtwitter")
-            #usrMsg=usrMsg+"[Twitter link] "
+        if "twitter.com" in words: #found twitter link
+            tweetFlag = True
+            newLink = words.replace("twitter","vxtwitter") #replaces twitter with vx twitter
+            if len(embedArr) > 1: #to check if there's more than one embed meaning there's multiple pictures
+                multiEmbedFlg = True
+                for i in embedArr:
+                    linkArr.append(i.image.url)
+                linkArr[0]=newLink # to make the first link that's sent the vxtwitter link
         else:
             usrMsg=usrMsg+words+" "
     if tweetFlag:
         await message.delete()
-        await message.channel.send(message.author.mention+": "+usrMsg+newLink)
-        #delete message from the usr who sent twitter link 
-        #message format:
-        #@{usr} said [usrMsg] \n newLink
+        if multiEmbedFlg:
+            await message.channel.send(message.author.display_name+": "+usrMsg+linkArr[0])
+            for i in range(1,len(embedArr)):
+                await message.channel.send(str(i+1)+"/"+str(len(embedArr)))
+                await message.channel.send(linkArr[i])
+        else:
+            await message.channel.send(message.author.display_name+": "+usrMsg+'\n'+newLink)
+            #delete message from the usr who sent twitter link 
+            #message format:
+            #{usr} said [usrMsg] \n newLink
+    else:
+        return
 
+client.run('token')
 
-client.run('Token')

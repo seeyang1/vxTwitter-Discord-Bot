92b6871f999fb67c3188c8f3dfa2749f72c80637
seeyang1 <51054781+seeyang1@users.noreply.github.com>
2023-12-19 00:03:46 -0500

    Update twitter_embedbot.py
    added support for instagram and tiktok as well as other names for twitter

--- twitter_embedbot.py
+++ twitter_embedbot.py
@@ -12,14 +12,99 @@ intents.message_content=True
 
 client = discord.Client(intents=intents)
 
-
 def twitterCheck(msgArr): #function that takes an array and returns true and false depending on if it contains a twitter link
-    #message parsing:           
+    #message parsing:
+    print("in the twitter checker")
+    print(msgArr)  
     for words in msgArr: # finding if a twitter link is sent, should technically be faster if there's no twitter link
-        if "twitter.com" in words:
-            return True
-        else:
+        #print(words)
+        if "vxtwitter.com" in words: #this means the fix has already been applied so no need to do anything
+            print("found a fix not doing shit")
             return False
+        elif "fixvx.com"in words: #this means the fix has already been applied so no need to do anything
+            print("found a fix not doing shit")
+            return False
+        elif "fixupx.com" in words: #this means the fix has already been applied so no need to do anything
+            print("found a fix not doing shit")
+            return False
+        elif "twitter.com" in words:
+            print("twitter found\n returning 1")
+            return 1
+        elif "x.com" in words:
+            print("x found\n returning 2")
+            return 2
+    print("exiting twitter checker, didn't find anything")
+    return False
+
+def instagramCheck(msgArr):
+    print("in the instagram checker")
+    for words in msgArr:
+        print("checking for: "+words)
+        if "ddinstagram.com" in words:
+            return False
+        elif "instagram.com" in words: 
+            return True
+    print("exiting insta checker, nothing found")
+    return False
+
+def tiktokCheck(msgArr):
+    print("in the tiktok checker")
+    for words in msgArr:
+        if "vxtiktok.com" in words: 
+            return False
+        elif "tiktok.com"in words:
+            return True
+    print("exiting insta checker, nothing found")
+    return False
+            
+def allCheck(msgArr):
+    print("In the allCheck funciton")
+    print(msgArr)
+    if twitterCheck(msgArr):
+        print("found a twitter link in using allCheck")
+        return True
+    elif instagramCheck(msgArr):
+        print("found an instagram link using allCheck")
+        return True
+    elif tiktokCheck(msgArr):
+        print("found a tiktok link using allCheck")
+        return True
+    else:
+        return False
+
+def embedFix(msgArr):
+    print("in embedFix function")
+    usrMsg=""
+    newLink=""
+    for words in msgArr:
+        print("in the for loop in the embedFix")
+        print(words)
+        if "twitter.com" in words:
+            print("yes twitter")
+            newLink = words.replace("twitter.com","vxtwitter.com")
+            print(newLink)
+        elif "x.com" in words:
+            print("yes x")
+            newLink = words.replace("x.com","fixvx.com") #replaces twitter with vxtwitter for videos
+            print(newLink)
+        elif "instagram.com" in words:
+            print("yes insta")
+            newLink = words.replace("instagram","ddinstagram") #replaces twitter with vxtwitter for videos
+            print(newLink)
+        elif "tiktok.com"in words:
+            print("yes tiktok")
+            newLink = words.replace("tiktok","vxtiktok") #replaces twitter with vxtwitter for videos
+            print(newLink)
+        else:
+            print("printing usr message variable:")
+            usrMsg=usrMsg+" "+words
+    print("printing new link and then user message")
+    print (newLink)
+    print (usrMsg)
+    newMsg = usrMsg+" \n"+newLink
+    print("printing new message")
+    print(newMsg)
+    return newMsg
 
 @client.event
 async def on_ready():
@@ -31,39 +116,17 @@ async def on_message(message):
         return
     else:
         x = re.split("\s",message.content)
-        #print(x)
-        if twitterCheck(x):
-            for words in x: #to go through the message that was sent 
-                if "vxtwitter" in words: #this means the fix has already been applied so no need to do anything
-                    return
-                if "twitter.com" in words: #found twitter link
-                    newLink=""
-                    usrMsg=""
-                    newDict={}
-                    vidFlag=False
-                    imgFlag=False
-                    embeds = message.embeds
-                    for embed in embeds:
-                        newDict=embed.to_dict()
-                    if newDict.get('video') is not None:
-                        vidFlag = True
-                    if newDict.get('image') is not None:
-                        imgFlag = True
-                    if vidFlag == False and imgFlag == False:
-                        return #don't need to apply fix as it's just a text tweet
-                    if vidFlag:
-                        newLink = words.replace("twitter","vxtwitter") #replaces twitter with vxtwitter for videos
-                    if imgFlag:
-                        newLink = words.replace("twitter","c.vxtwitter") #replaces twitter with c.vxtwitter in case there are multiple images
-                else:
-                    usrMsg=usrMsg+words+" " # here just in case somebody sends text alongside the twitter link
+        print("parting the red sea")
+        print(x)
+        if allCheck(x):
+            print("in the all check loop")
             await message.delete() # work flow: delete original message -> send the fixed media message(s)
-            await message.channel.send(message.author.display_name+": "+usrMsg+'\n'+newLink)
+            await message.channel.send(message.author.display_name+":"+embedFix(x))
             #message format: 
             #{usr}: [usrMsg] newLink
             # [Other pictures if applicable]
             return 
-        else:
-            return
+        return
+
 
 client.run('TOKEN')
